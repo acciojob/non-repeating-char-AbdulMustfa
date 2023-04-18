aff6e42ff2df2fe1797d652cd6c5723c23f2ae1c
Abdul Mustafa <117674574+AbdulMustfa@users.noreply.github.com>
2023-04-18 14:00:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-//your JS code here. If required.
-function findFirstNonRepeatedChar(str) {
-  for (let i = 0; i < str.length; i++) {
-    let char = str.charAt(i);
-    if (str.indexOf(char) == i && str.indexOf(char, i + 1) == -1) {
+function firstNonRepeatedChar() {
+  const inputStr = prompt("Enter a string:");
+
+  const freq = {};
+  for (let char of inputStr) {
+    freq[char] = (freq[char] || 0) + 1;
+  }
+
+  for (let char of inputStr) {
+    if (freq[char] === 1) {
+      
+      alert(`The first non-repeated character is ${char}.`);
       return char;
     }
   }
+
+
+  alert("There is no non-repeated character in the string.");
   return null;
 }
 
-let input = prompt("Enter a string:");
-let firstNonRepeatedChar = findFirstNonRepeatedChar(input);
-
-if (firstNonRepeatedChar) {
-  alert("The first non-repeated character is: " + firstNonRepeatedChar);
-} else {
-  alert("There are no non-repeated characters in the string.");
-}
-
+firstNonRepeatedChar();
